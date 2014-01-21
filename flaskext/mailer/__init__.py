f6ffcc75ecaa165f140798557f3bda427da2d151
Vital Kudzelka <vital.kudzelka@gmail.com>
2014-01-21 13:37:24 +0300

    remove legacy prefixes from extensions

--- flaskext/mailer/__init__.py
+++ flaskext/mailer/__init__.py
@@ -9,19 +9,17 @@ from flask.ext.mailer.util import import_path
 
 __all__ = ('get_mailer', 'send_email', 'Mailer', 'Email')
 
-DEFAULT_PREFIX = 'MAILER'
 
-
-def send_email(subject, text, to_addrs, fail_quiet=True, prefix=DEFAULT_PREFIX):
+def send_email(subject, text, to_addrs, fail_quiet=True):
     """Send an email."""
-    mailer = get_mailer(prefix)
+    mailer = get_mailer(None)
     mail = Email(subject, text, to_addrs)
     if fail_quiet:
         return mailer.send_quiet(mail)
     return mailer.send(mail)
 
 
-def get_mailer(prefix=DEFAULT_PREFIX):
+def get_mailer():
     """Returns mailer for current app or raise RuntimeError."""
     app = current_app
 
@@ -29,13 +27,10 @@ def get_mailer(prefix=DEFAULT_PREFIX):
        'mailer' not in app.extensions:
         raise RuntimeError('Mailer extension does not registered')
 
-    mailer = app.extensions['mailer'].get(prefix)
-    if mailer is None:
-        raise RuntimeError('Mailer with prefix "%s" does not exist' % prefix)
-    return mailer
+    return app.extensions['mailer']
 
 
-def init_mailer(prefix, config=None):
+def init_mailer(config=None):
     """Create new mailer from config."""
     config = config or {}
     path = config.pop(key('backend'), None)
@@ -49,20 +44,13 @@ def init_mailer(prefix, config=None):
 class Mailer(object):
     """Mailer instance manages sending of email messages."""
 
-    def __init__(self, app=None, prefix=DEFAULT_PREFIX):
+    def __init__(self, app=None):
         if app is not None:
-            self.init_app(app, prefix)
+            self.init_app(app)
 
-    def init_app(self, app, prefix=DEFAULT_PREFIX):
+    def init_app(self, app):
         # register extension themselves for backwards compatibility
         app.extensions = getattr(app, 'extensions', {})
-        app.extensions['mailer'] = getattr(app.extensions, 'mailer', {})
-        if prefix in app.extensions['mailer']:
-            raise RuntimeError('duplicate config prefix: "%s"' % prefix)
-
-        self.app = app
-        self.prefix = prefix
-
 
         # set default settings
         config = app.config
@@ -79,16 +67,16 @@ class Mailer(object):
         if config[key('testing')]:
             config[key('backend')] = 'flask.ext.mailer.backends.dummy.DummyMailer'
 
-        mailer = init_mailer(prefix, config)
-        app.extensions['mailer'][prefix] = mailer
+        state = init_mailer(config)
+        app.extensions['mailer'] = state
 
     def send(self, mail):
         """Send the email message."""
-        get_mailer(self.prefix).send(mail)
+        get_mailer(self.state).send(mail)
 
     def send_quiet(self, mail):
         """Send the email message but swallow exceptions."""
-        get_mailer(self.prefix).send_quiet(mail)
+        get_mailer(self.state).send_quiet(mail)
 
 
 if __name__ == '__main__':
