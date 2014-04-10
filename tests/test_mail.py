3e0509fe980b7e63c9d2b8d0c78a01d795941750
Vital Kudzelka <vital.kudzelka@gmail.com>
2014-04-10 13:26:11 +0300

    Use class to wrap mail test suite

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -54,72 +54,78 @@ class TestAddress:
         assert Address(addr) == addr
 
 
-def test_mail_init(mail):
-    assert mail.subject == 'Down the Rabbit-Hole'
-    assert mail.text == 'What is the use of a book without pictures or conversation?'
-    assert mail.from_addr == 'Alice from Wonderland <alice@wonderland.com>'
-    assert mail.to_addrs == ['one@example.com', 'two@example.com']
-    assert mail.cc == ['cc@example.com']
-    assert mail.bcc == ['bcc@example.com']
-    assert mail.reply_to == 'noreply@wonderland.com'
+class TestMail:
 
+    def test_mail_init(self, mail):
+        assert mail.subject == 'Down the Rabbit-Hole'
+        assert mail.text == 'What is the use of a book without pictures or conversation?'
+        assert mail.from_addr == 'Alice from Wonderland <alice@wonderland.com>'
+        assert mail.to_addrs == ['one@example.com', 'two@example.com']
+        assert mail.cc == ['cc@example.com']
+        assert mail.bcc == ['bcc@example.com']
+        assert mail.reply_to == 'noreply@wonderland.com'
 
-def test_send_mail_to_cc_bcc_addresses(mail):
-    assert mail.send_to == set(['bcc@example.com', 'cc@example.com', 'one@example.com', 'two@example.com'])
-    mail.add_addr('somebody@example.com')
-    assert mail.send_to == set(['bcc@example.com', 'cc@example.com', 'one@example.com', 'two@example.com', 'somebody@example.com'])
+    def test_from_addr(self, mail):
+        mail.from_addr = 'alice@example.com'
+        assert mail.from_addr == 'alice@example.com'
 
+    def test_unpack_from_addr(self, mail):
+        mail.from_addr = ['Alice', 'alice@example.com']
+        assert mail.from_addr == 'Alice <alice@example.com>'
 
-def test_no_duplicate_entries_in_recipients_list(mail):
-    assert len(mail.send_to) == 4
-    assert 'cc@example.com' in mail.send_to
-    mail.add_addr('cc@example.com')
-    assert len(mail.send_to) == 4
+    def test_reply_to(self, mail):
+        mail.reply_to = 'noreply@example.com'
+        assert mail.reply_to == 'noreply@example.com'
 
+    def test_unpack_reply_to(self, mail):
+        mail.reply_to = ('Alice', 'noreply@example.com')
+        assert mail.reply_to == 'Alice <noreply@example.com>'
 
-def test_mail_to_addr_cc_bcc_as_string():
-    mail = Email('hello', to_addrs='to@example.com',
-                 cc='cc@example.com', bcc='bcc@example.com')
-    assert mail.send_to == set(['to@example.com', 'cc@example.com', 'bcc@example.com'])
+    def test_send_to_includes_cc_and_bcc_addresses_too(self, mail):
+        assert mail.send_to == set(['bcc@example.com', 'cc@example.com', 'one@example.com', 'two@example.com'])
+        mail.add_addr('somebody@example.com')
+        assert mail.send_to == set(['bcc@example.com', 'cc@example.com', 'one@example.com', 'two@example.com', 'somebody@example.com'])
 
+    def test_no_duplicate_entries_in_recipients_list(self, mail):
+        assert len(mail.send_to) == 4
+        assert 'cc@example.com' in mail.send_to
+        mail.add_addr('cc@example.com')
+        assert len(mail.send_to) == 4
 
-def test_mail_to_addr_cc_bcc_as_list():
-    mail = Email('hello', to_addrs=['to@example.com'],
-                 cc=['cc@example.com'], bcc=['bcc@example.com'])
-    assert mail.send_to == set(['to@example.com', 'cc@example.com', 'bcc@example.com'])
+    def test_to_addr_cc_bcc_as_string(self):
+        mail = Email('hello', to_addrs='to@example.com',
+                     cc='cc@example.com', bcc='bcc@example.com')
+        assert mail.send_to == set(['to@example.com', 'cc@example.com', 'bcc@example.com'])
 
+    def test_to_addr_cc_bcc_as_list(self):
+        mail = Email('hello', to_addrs=['to@example.com'],
+                     cc=['cc@example.com'], bcc=['bcc@example.com'])
+        assert mail.send_to == set(['to@example.com', 'cc@example.com', 'bcc@example.com'])
 
-def test_mail_unpack_from_addr():
-    mail = Email('hello', from_addr=('me', 'me@example.com'))
-    assert mail.from_addr == 'me <me@example.com>'
+    def test_add_destination_address_to_mail(self, mail):
+        mail.add_addr('hatter@wonderland.com')
+        assert 'hatter@wonderland.com' in mail.to_addrs
 
+    def test_raises_error_if_mailing_parameters_is_blank(self):
+        mail = Email('Dummy mail')
+        with pytest.raises(RuntimeError) as err:
+            mail.to_message()
+            assert err.message == 'Fill in mailing parameters first'
 
-def test_add_destination_address_to_mail(mail):
-    mail.add_addr('hatter@wonderland.com')
-    assert 'hatter@wonderland.com' in mail.to_addrs
+    def test_mail_to_mimetext(self, mail):
+        message = mail.to_message()
+        assert message['From'] == 'Alice from Wonderland <alice@wonderland.com>'
+        assert message['To'] == 'bcc@example.com, cc@example.com, one@example.com, two@example.com'
+        assert message['Subject'] == 'Down the Rabbit-Hole'
+        assert message['Cc'] == 'cc@example.com'
+        assert message['Bcc'] == 'bcc@example.com'
+        assert message['Reply-To'] == 'noreply@wonderland.com'
+        assert message['Content-Type'] == 'text/plain; charset=utf-8'
+        assert message['Content-Transfer-Encoding'] == '8bit'
 
-
-def test_mail_raises_error_if_mailing_parameters_is_blank():
-    mail = Email('Dummy mail')
-    with pytest.raises(RuntimeError) as err:
-        mail.to_message()
-        assert err.message == 'Fill in mailing parameters first'
-
-
-def test_mail_to_mimetext(mail):
-    message = mail.to_message()
-    assert message['From'] == 'Alice from Wonderland <alice@wonderland.com>'
-    assert message['To'] == 'bcc@example.com, cc@example.com, one@example.com, two@example.com'
-    assert message['Subject'] == 'Down the Rabbit-Hole'
-    assert message['Cc'] == 'cc@example.com'
-    assert message['Bcc'] == 'bcc@example.com'
-    assert message['Reply-To'] == 'noreply@wonderland.com'
-    assert message['Content-Type'] == 'text/plain; charset=utf-8'
-    assert message['Content-Transfer-Encoding'] == '8bit'
-
-
-def test_mail_to_string(mail):
-    assert mail.format(sep='\n') == '''MIME-Version: 1.0
+    def test_mail_to_string(self, mail):
+        assert mail.format(sep='\n') == '''\
+MIME-Version: 1.0
 From: Alice from Wonderland <alice@wonderland.com>
 To: bcc@example.com, cc@example.com, one@example.com, two@example.com
 Subject: Down the Rabbit-Hole
