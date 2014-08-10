a6ec57192a915485cef4276a95d83017b08f612d
Vital Kudzelka <vital.kudzelka@gmail.com>
2014-08-10 21:01:46 +0300

    Application factory converts option keys to upper case

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,15 +6,18 @@ from flask import Flask
 
 
 def create_app(**options):
-    """Create a Flask instance.
+    """Create a Flask instance. Converts option keys to upper case.
 
     :param **options: The additional application options.
     """
     app = Flask(__name__)
-    app.config.update(options)
+
+    for name, value in options.items():
+        app.config[name.upper()] = value
+
     return app
 
 
 @pytest.fixture
 def app():
-    return create_app(TESTING=True)
+    return create_app(testing=True)
