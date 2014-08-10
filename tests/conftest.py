7b1f20756ccd53852ae7bac6be54a6f9510b3778
Vital Kudzelka <vital.kudzelka@gmail.com>
2014-08-10 21:12:09 +0300

    Use `pytest.mark` decorator to set extension options

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@
 import pytest
 
 from flask import Flask
+from flask.ext.mailer.util import key
 
 
 def create_app(**options):
@@ -19,5 +20,30 @@ def create_app(**options):
 
 
 @pytest.fixture
-def app():
-    return create_app(testing=True)
+def app(request):
+    """Use `pytest.mark` decorator to pass options to your application
+    factory::
+
+        @pytest.mark.app(static_folder='assets')
+        def test_app(app):
+            pass
+
+    Set options to extension, e.g.::
+
+        @pytest.mark.config(foo=42)
+        def test_app(app):
+            pass
+
+    """
+    options = dict(debug=True, testing=True, secret='secret')
+
+    if 'app' in request.keywords:
+        options.update(request.keywords['app'].kwargs)
+
+    app = create_app(**options)
+
+    if 'config' in request.keywords:
+        for name, value in request.keywords['config'].kwargs.items():
+            app.config[key(name)] = value
+
+    return app
