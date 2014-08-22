c0444c3898b0531804247c536e91f92e012d2a50
Vital Kudzelka <vital.kudzelka@gmail.com>
2014-08-22 16:51:58 +0300

    Add decorator to define `__str__` and `__unicode__` methods under py2

--- flaskext/mailer/compat.py
+++ flaskext/mailer/compat.py
@@ -8,8 +8,22 @@ if sys.version_info[0] >= 3:
     string_types = str,
     iteritems = lambda o: o.items()
     itervalues = lambda o: o.values()
+
+    unicode_compatible = lambda x: x
 else:
     text_type = unicode
     string_types = basestring,
     iteritems = lambda o: o.iteritems()
     itervalues = lambda o: o.itervalues()
+
+    def unicode_compatible(cls):
+        """A decorator which defines `__str__` and `__unicode__` methods in
+        decorated class.
+        """
+        if '__str__' not in cls.__dict__:
+            raise ValueError('decorator cannot be applied to %s '
+                             'because it does not define __str__ method' %
+                             cls.__name__)
+        cls.__unicode__ = cls.__str__
+        cls.__str__ = lambda x: x.__unicode__().encode('utf-8')
+        return cls
